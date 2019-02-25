Fix plotting variance with ellipse

main.py
@@ -6,7 +6,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 import mlflow
-from sklearn.decomposition import PCA
+# from sklearn.decomposition import PCA
 
 from common.dataset import dataset
 import common.plot.simple_plot
@@ -28,35 +28,32 @@ def scatter_with_images(fig, z2d, z2d_scale, original_data, labels_true, name):
     plt.close()
 
 
-def scatter_with_errors(fig, z2d_loc, z2d_scale, labels_true, name):
+def scatter_with_errors(fig, z2d_loc, z2d_scale, labels_true, name, K=10):
     def _scatter():
         _, ax = plt.subplots(1, 1, figsize=(10, 10))
-        ax.set_xlim(-0.5, 0.5)
-        ax.set_ylim(-1, 1)
-        ax.scatter(z2d_loc[:, 0], z2d_loc[:, 1], c=labels_true)
-        ellipse_scatter(ax, z2d_loc, 0.1 * z2d_scale, labels_true)
-    fig.size(None).name(name).plot(_scatter)
-    plt.close()
 
+        # for each point in 2D, use the norm of variance (scale_x and scale_y)
+        # as the 'error' and show them in the viz
+        errors = 2000 * np.linalg.norm(z2d_scale, axis=1)
 
-def scatter_errors(fig, z2d_loc, z2d_scale, labels_true, name, K=10):
-    def _scatter():
-        # errors = list(map(lambda p: 1000*np.linalg.norm(p), z2d_scale[:, :]))
-        errors = 1000 * np.linalg.norm(z2d_scale, axis=1)
-        _, ax = plt.subplots(1,1, figsize=(10,10))
-        ax.scatter(z2d_loc[:, 0], z2d_loc[:, 1], c=labels_true, s=errors, alpha=0.5)
+        # draw variance of each points by an ellipse
+        ellipse_scatter(ax, z2d_loc, 5 * z2d_scale, labels_true, alpha=0.1)
 
         # get top 10 errors and highlight them
         ind = np.argpartition(errors, -K)[-K:]
         highlights = z2d_loc[ind]
         ax.scatter(highlights[:, 0], highlights[:, 1], c='w',
-                   linewidths=2., edgecolors='red')
+                   linewidths=2., edgecolors='red', s=errors[ind])
+
+        # scatter the points with size of each point is its norm of variance
+        ax.scatter(z2d_loc[:, 0], z2d_loc[:, 1], c=labels_true, s=errors,
+                   alpha=0.5, linewidths=0, edgecolors='w', cmap='tab10')
+
     fig.size(None).name(name).plot(_scatter)
     plt.close()
 
 
-def run_ppca(X_original, X, y, learning_rate, n_iters,
-             plot_args={}, plot_errors=False):
+def run_ppca(X_original, X, y, learning_rate, n_iters, plot_args={}):
     losses, z2d_loc, z2d_scale = MAP(X, learning_rate, n_iters)
 
     sum_vars = np.linalg.norm(z2d_scale, axis=1).sum()
@@ -68,19 +65,19 @@ def run_ppca(X_original, X, y, learning_rate, n_iters,
     fig = SimpleFigure(**plot_args)
     fig.size((6, 3)).name('losses').plot(lambda: plt.plot(losses))
 
-    # if not plot_errors:
     scatter_with_images(fig, z2d_loc, z2d_scale, X_original, y, name='z2d')
-    # else:
-    scatter_errors(fig, z2d_loc, z2d_scale, y, 'z2d_error')
+    scatter_with_errors(fig, z2d_loc, z2d_scale, y, 'z2d_error')
 
 
 def nested_run(n_iters, learning_rates, datasets, preprocessing_method):
     for dataset_name in datasets:
         with mlflow.start_run():
-            mlflow.log_param('dataset_name', f"{dataset_name}_{preprocessing_method}")
+            mlflow.log_param('dataset_name',
+                             f"{dataset_name}_{preprocessing_method}")
             mlflow.log_param('n_iters', n_iters)
 
-            X_original, X, y = dataset.load_dataset(dataset_name, preprocessing_method)
+            X_original, X, y = dataset.load_dataset(dataset_name,
+                                                    preprocessing_method)
             X = torch.tensor(X, dtype=torch.float)
 
             for lr in learning_rates:
@@ -109,7 +106,7 @@ def simple_run(dataset_name, n_iters, lr):
 
     X_original, X, y = dataset.load_dataset(dataset_name)
     X = torch.tensor(X, dtype=torch.float)
-    run_ppca(X_original, X, y, lr, n_iters, plot_args, plot_errors=True)
+    run_ppca(X_original, X, y, lr, n_iters, plot_args)
 
 
 if __name__ == '__main__':
@@ -125,10 +122,10 @@ if __name__ == '__main__':
 
     mlflow.set_experiment('PPCA_pyro')
 
-    learning_rates = [0.005, 0.01, 0.015, 0.02, 0.025, 0.005, 0.075, 0.1, 0.15, 0.2]
-    n_iters = 350
-    datasets = ['FASHION200']
-    preprocessing_method = 'standardize' # 'no_preprocess'
-    nested_run(n_iters, learning_rates, datasets, preprocessing_method)
+#    learning_rates = [0.005, 0.01, 0.02, 0.025, 0.005, 0.075, 0.1, 0.15, 0.2]
+#    n_iters = 350
+#    datasets = ['FASHION200']
+#    preprocessing_method = 'standardize' # 'no_preprocess'
+#    nested_run(n_iters, learning_rates, datasets, preprocessing_method)
 
-    # simple_run(dataset_name='FASHION200', n_iters=250, lr=0.2)
+    simple_run(dataset_name='FASHION200', n_iters=250, lr=0.2)
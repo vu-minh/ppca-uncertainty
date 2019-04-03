9656683d250a5ab7b31533045d2665059ea00542
vu-minh <vuvietminh@unamur.be>
2019-04-03 10:02:36 +0200

    Update deep model with one hidden layer

--- ppca_model2.py
+++ ppca_model2.py
@@ -15,54 +15,50 @@ from pyro.infer import SVI, Trace_ELBO, TraceEnum_ELBO, config_enumerate
 from tqdm import tqdm
 from functools import partial
 from tensorboardX import SummaryWriter
+from matplotlib import pyplot as plt
+from common.dataset import dataset
+from common.plot.scatter import imscatter
+import math
+import argparse
 
+
+dataset.set_data_home("./data")
 writer = SummaryWriter()
 
 
 class DeepPPCAModel(nn.Module):
-    def __init__(self, n_data_dims, n_latent_dims=2):
+    def __init__(self, data_dim=784, z_dim=2, hidden_dim=300):
         super(DeepPPCAModel, self).__init__()
-        self.n_data_dims = n_data_dims
-        self.n_latent_dims = n_latent_dims
-        self.linear1 = nn.Linear(
-            in_features=n_latent_dims, out_features=n_data_dims, bias=True
-        )
+
+        self.fc1 = nn.Linear(z_dim, hidden_dim)
+        self.fc21 = nn.Linear(hidden_dim, data_dim)
+        self.softplus = nn.Softplus()
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, z):
-        return self.linear1(z)
+        hidden = self.softplus(self.fc1(z))
+        loc_img = self.softplus(self.fc21(hidden))
+        return loc_img
 
 
-def deep_ppca_model(data, M=2, moved_points={}, sigma_fix=1e-3):
-    N, D = data.shape
+def deep_ppca_model(data, hidden_dim=300, z_dim=2, moved_points={}, sigma_fix=1e-3):
+    N, data_dim = data.shape
 
-    deep_ppca = DeepPPCAModel(n_data_dims=D, n_latent_dims=M)
-
-    alpha = pyro.sample(
-        "alpha", LogNormal(torch.zeros(1, M), torch.ones(1, M)).to_event(2)
-    )
-    w_prior = pyro.distributions.Normal(
-        torch.zeros(D, M), torch.ones(D, M) * alpha
-    ).to_event(2)
-    bias_prior = pyro.distributions.Normal(
-        torch.zeros(1, D), torch.ones(1, D)
-    ).to_event(1)
-    priors = {
-        "linear1.weight": w_prior,
-        # "linear1.bias": bias_prior
-    }
-
-    lifted_module = pyro.random_module("deep_ppca", deep_ppca, priors)
-    lifted_ppca_model = lifted_module()
+    deep_ppca = DeepPPCAModel(data_dim=data_dim, z_dim=z_dim, hidden_dim=hidden_dim)
+    pyro.module("deep_ppca_decoder", deep_ppca)
 
     sigma = pyro.sample(
-        "sigma", LogNormal(torch.zeros(1, D), torch.ones(1, D)).to_event(1)
+        "sigma",
+        LogNormal(torch.zeros(1, data_dim), torch.ones(1, data_dim)).to_event(1),
     )
 
     with pyro.plate("data_plate", N):
-        Z = pyro.sample("Z", Normal(torch.zeros(N, M), torch.ones(N, M)).to_event(1))
+        Z = pyro.sample(
+            "Z", Normal(torch.zeros(N, z_dim), torch.ones(N, z_dim)).to_event(1)
+        )
 
-        generated_X_mean = lifted_ppca_model(Z)
-        generated_X_scale = torch.ones(N, D) * sigma
+        generated_X_mean = deep_ppca.forward(Z)
+        generated_X_scale = torch.ones(N, data_dim) * sigma
 
         obs = pyro.sample(
             "obs", Normal(generated_X_mean, generated_X_scale).to_event(1), obs=data
@@ -70,54 +66,49 @@ def deep_ppca_model(data, M=2, moved_points={}, sigma_fix=1e-3):
         return obs
 
 
-def trainVI(data, learning_rate=1e-03, n_iters=500, trace_embeddings_interval=20):
+def trainVI(
+    data, hidden_dim, learning_rate=1e-03, n_iters=500, trace_embeddings_interval=20
+):
     pyro.enable_validation(True)
     pyro.set_rng_seed(0)
     pyro.clear_param_store()
 
-    model = partial(deep_ppca_model, M=2)
+    model = partial(deep_ppca_model, hidden_dim=hidden_dim, z_dim=2)
+
     guide = AutoGuideList(model)
-    guide.add(
-        AutoDiagonalNormal(
-            model=poutine.block(
-                model,
-                expose=[
-                    "alpha",
-                    "sigma",
-                    "deep_ppca$$$linear1.weight",
-                    # "deep_ppca$$$linear1.bias",
-                ],
-            ),
-            prefix="q",
-        )
-    )
-    guide.add(AutoDiagonalNormal(model=poutine.block(model, expose=["Z"])))
+    guide.add(AutoDiagonalNormal(model=poutine.block(model, expose=["sigma"])))
+    guide.add(AutoDiagonalNormal(model=poutine.block(model, expose=["Z"]), prefix="qZ"))
 
     optim = Adam({"lr": learning_rate})
     svi = SVI(model, guide, optim, loss=Trace_ELBO())
 
+    fig_title = f"lr={learning_rate}/hidden-dim={hidden_dim}"
     data = torch.tensor(data, dtype=torch.float)
     for n_iter in tqdm(range(n_iters)):
         loss = svi.step(data)
-        writer.add_scalar("train_vi/loss", loss, n_iter)
+        if n_iter % 10 == 0:
+            writer.add_scalar("train_vi/loss", loss, n_iter)
 
         if trace_embeddings_interval and n_iter % trace_embeddings_interval == 0:
-            z2d_loc = pyro.param("auto_loc").reshape(-1, 2).data.numpy()
-            fig = get_fig_plot_z2d(z2d_loc)
+            z2d_loc = pyro.param("qZ_loc").reshape(-1, 2).data.numpy()
+            fig = get_fig_plot_z2d(z2d_loc, fig_title)
             writer.add_figure("train_vi/z2d", fig, n_iter)
 
     # show named rvs
-    # print("List params", pyro.get_param_store().keys())
+    print("List params and their size: ")
+    for p_name, p_val in pyro.get_param_store().items():
+        print(p_name, p_val.shape)
 
-    z2d_loc = pyro.param("auto_loc").reshape(-1, 2).data.numpy()
-    z2d_scale = pyro.param("auto_scale").reshape(-1, 2).data.numpy()
-    fig = get_fig_plot_z2d(z2d_loc)
+    z2d_loc = pyro.param("qZ_loc").reshape(-1, 2).data.numpy()
+    z2d_scale = pyro.param("qZ_scale").reshape(-1, 2).data.numpy()
+    fig = get_fig_plot_z2d(z2d_loc, fig_title)
     writer.add_figure("train_vi/z2d", fig, n_iters)
     return z2d_loc, z2d_scale
 
 
-def get_fig_plot_z2d(z2d):
+def get_fig_plot_z2d(z2d, title):
     fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+    ax.set_title(title)
     ax.scatter(z2d[:, 0], z2d[:, 1], alpha=0.5)
     imscatter(ax, z2d, X_original, zoom=0.5)
     return fig
@@ -131,24 +122,31 @@ def create_tensorboard_embedding(data, labels, features=None):
         features if features is not None else images,
         metadata=labels,
         label_img=images.view(N, 1, img_size, img_size),
-        tag=dataset_name,
+        tag=args.dataset_name,
     )
 
 
 if __name__ == "__main__":
-    from matplotlib import pyplot as plt
-    from common.dataset import dataset
-    from common.plot.scatter import imscatter
-    import math
+    ap = argparse.ArgumentParser(description="DeepPPCAModel")
+    ap.add_argument("-d", "--dataset_name", default="")
+    ap.add_argument("-x", "--dev", action="store_true")
+    ap.add_argument("-lr", "--learning_rate", default=1e-3, type=float)
+    ap.add_argument("-s", "--scale_data", default="unitScale")
+    ap.add_argument("-n", "--n_iters", default=1000, type=int)
+    ap.add_argument("-hd", "--hidden_dim", default=300, type=int)
 
-    dataset.set_data_home("./data")
-    dataset_name = "FASHION200"
+    args = ap.parse_args()
+    writer.add_text(f"Params", str(args))
 
     X_original, X, y = dataset.load_dataset(
-        dataset_name, preprocessing_method="unitScale"
+        args.dataset_name, preprocessing_method=args.scale_data
     )
     z2d_loc, z2d_scale = trainVI(
-        data=X, learning_rate=0.1, n_iters=1000, trace_embeddings_interval=25
+        data=X,
+        hidden_dim=args.hidden_dim,
+        learning_rate=args.learning_rate,
+        n_iters=5 if args.dev else args.n_iters,
+        trace_embeddings_interval=100,
     )
 
     # create_tensorboard_embedding(data=X_original, labels=y, features=z2d_loc)
